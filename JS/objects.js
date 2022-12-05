80e1f7ed812f50d18081f5c4170ce28830ae1471
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2022-12-05 14:16:29 -0600

    1

---
+++ JS/objects.js
@@ -0,0 +1,35 @@
+const rey = Object();
+
+
+
+rey.petType = "dog";
+rey.breed = "german shepherd";
+console.log(rey);
+
+console.log(rey.breed);
+
+
+const dolly = {
+    name: "Dolly",
+    gender: "female",
+    breed: "american bulldog"
+}
+
+
+console.log(dolly)
+
+
+const candy = {
+    name: 'Candy',
+    petType: 'dog',
+    gender: 'female',
+    breen: 'pug',
+    age: '2',
+    owner: {
+        name: 'Joe',
+        address: '555 Sesame Street',
+        Phone: '2105555555'
+
+    }
+}
+console.log(candy)
